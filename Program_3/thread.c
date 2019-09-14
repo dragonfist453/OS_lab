e275c98139103b46c7e9b0190b0ea0979c0332f7
dragonfist453 <thermosphere453@gmail.com>
2019-09-14 16:59:48 +0530

    Program 3

--- Program_3/thread.c
+++ Program_3/thread.c
@@ -4,22 +4,29 @@
 #include <unistd.h>
 #include <sys/stat.h>
 #include <sys/types.h>
-int num;
-void *factorial(void *param)
+#include <string.h>
+long unsigned int prod=1;
+void *odd_multiplier(void *param)
 {
-	num = atoi(param);
-	int i=1;
-	long int prod=1;
-	for(i=1;i<=num;i++)
+	int n = atoi(param);
+	int i;
+	for(i=1;i<=n;i+=2)
 		prod*=i;
-	printf("Factorial of %d is %ld\n",num,prod);
+	pthread_exit(0);
+}
+void *even_multiplier(void *param)
+{
+	int n = atoi(param);
+	int i;
+	for(i=2;i<=n;i+=2)
+		prod*=i;
 	pthread_exit(0);
 }
 int main(int argc,char* argv[])
 {
-	pthread_t tid;
-	pthread_attr_t attr;
-	if(argc!=2)
+	pthread_t odd,even;
+	//pthread_attr_t attr;
+	if(argc!=2 || strcasecmp("HELP",argv[1])==0)
 	{
 		fprintf(stderr,"usage: a.out <integer value>\n");
 		return -1;
@@ -29,8 +36,11 @@ int main(int argc,char* argv[])
 		fprintf(stderr,"%d must be >= 0\n",atoi(argv[1]));
 		return -1;
 	}
-	pthread_attr_init(&attr);
-	pthread_create(&tid,&attr,factorial,argv[1]);
-	pthread_join(tid,NULL);
+	//pthread_attr_init(&attr);
+	pthread_create(&odd,NULL,odd_multiplier,argv[1]);
+	pthread_create(&even,NULL,even_multiplier,argv[1]);
+	pthread_join(odd,NULL);
+	pthread_join(even,NULL);
+	printf("Factorial of %d is %lu\n",atoi(argv[1]),prod);
 	return 0;
 }
