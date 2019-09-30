2258a3985a97507f64b4c22df7ef40a50cf42558
dragonfist453 <thermosphere453@gmail.com>
2019-10-01 00:17:43 +0530

    Program 5 all done

--- Program_5/dinphil.c
+++ Program_5/dinphil.c
@@ -20,8 +20,8 @@ void test(int phnum)
 	{
 		state[phnum] = eating;
 		sleep(2);
-		printf("Philosopher %d takes chopsticks %d and %d\n",phnum+i,left+i,phnum+i);
-		printf("Philosopher %d is Eating\n",phnum+i);
+		printf("Philosopher %d takes chopsticks %d and %d\n",phnum+1,left+1,phnum+1);
+		printf("Philosopher %d is Eating\n",phnum+1);
 		sem_post(&S[phnum]);
 	}
 }
@@ -29,5 +29,47 @@ void take_fork(int phnum)
 {
 	sem_wait(&mutex);
 	state[phnum] = hungry;
-	printf
+	printf("Philosopher %d is Hungry\n",phnum+1);
+	test(phnum);
+	sem_post(&mutex);
+	sem_wait(&S[phnum]);
+	sleep(1);
+}
+void put_fork(int phnum)
+{
+	sem_wait(&mutex);
+	state[phnum]=thinking;
+	printf("Philosopher %d putting chopsticks %d and %d down\n",phnum+1,left+1,phnum+1);
+	printf("Philosopher %d is thinking\n",phnum+1);
+	test(left);
+	test(right);
+	sem_post(&mutex);
+}
+void *philosopher(void *num)
+{
+	while(1)
+	{
+		int *i = num;
+		sleep(1);
+		take_fork(*i);
+		sleep(0);
+		put_fork(*i);
+	}
+}
+int main()
+{
+	int i;
+	pthread_t thread_id[size];
+	sem_init(&mutex,0,1);
+	for(i=0;i<size;i++)
+		sem_init(&S[i],0,0);
+	for(i=0;i<size;i++)
+	{
+		pthread_create(&thread_id[i],NULL,philosopher,&phil[i]);
+		printf("Philosopher %d is thinking\n",i+1);
+	}
+	for(i=0;i<size;i++)
+		pthread_join(thread_id[i],NULL);
+	return 0;		
+}
 
